6ad5bf6bdba7e07d5b837f5e3ec5f56bff041c62
Nathan Emery <33710782+nlemery@users.noreply.github.com>
2017-11-19 07:31:54 -0800

    Create morning.ps1

---
+++ morning.ps1
@@ -0,0 +1,8 @@
+Start-Process -FilePath Firefox -ArgumentList "https://mail.google.com/mail/u/0/#inbox", https://www.facebook.com/, https://www.npr.org/
+Start-Process 'c:\program files\explorer.exe shell:WindowsApps\Microsoft.BingWeather_4.21.2212.0_neutral_split.scale-100_8wekyb3d8bbwe'
+
+# Opens email, social media and morning reading in separate tabs in Firefox. Opens MSN Weather app. 
+# Load script into startup to run when Windows loads, make coffee while laptop finished booting and pages load. 
+
+# Revised 11-10-17: Three lines to open websites condensed into one line.
+# Revised 11-18-17: Added MSN Weather app to script.
